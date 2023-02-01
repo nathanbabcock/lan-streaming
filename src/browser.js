f42ab9f6e0136c83aae190a387da67c31a26d77c
nbabcock <nathan.r.babcock@gmail.com>
2023-02-01 10:19:44 -0600

    feat: regenerate https cert (invalid ca)

--- src/browser.js
+++ src/browser.js
@@ -2,13 +2,13 @@ console.log('Hello world!')
 
 async function main() {
   // Hardcoded; fix CORS to get this from local server
-  const hash = Buffer.from(JSON.stringify({
-    "type": "Buffer",
-    "data": [
-      108, 9, 14, 111, 70, 49, 150, 149, 152, 116, 254, 232, 119, 227, 120, 86,
-      118, 141, 149, 68, 132, 11, 251, 248, 238, 46, 52, 93, 72, 244, 163, 99
-    ]
-  }))
+
+  const hash = new Uint8Array([
+    108, 9, 14, 111, 70, 49, 150, 149, 152, 116, 254, 232, 119, 227, 120, 86,
+    118, 141, 149, 68, 132, 11, 251, 248, 238, 46, 52, 93, 72, 244, 163, 99
+  ]).buffer
+
+  console.log(hash.byteLength)
 
   const url = 'https://127.0.0.1:3001/echo'
   console.log(`[webtransport] Connecting to ${url}`)
